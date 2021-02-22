4fcd642f4d7feefaf955ff6f9d36a9ff21a04bd7
fdavalo <57140104+fdavalo@users.noreply.github.com>
2021-02-22 13:35:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import process from 'process';
-
-const watch = require('watch.js');
+import {Watch} from './watch.js';
 
 process.on('uncaughtException', function (err) {
 	console.log(err);
@@ -14,4 +13,4 @@ if (process.argv.length > 2) resource = process.argv[2];
 if (process.argv.length > 3) tokendir = process.argv[3];
 if (process.argv.length > 4) cadir = process.argv[4];
 
-new watch.Watch(resource, tokendir, cadir);
+new Watch(resource, tokendir, cadir);
